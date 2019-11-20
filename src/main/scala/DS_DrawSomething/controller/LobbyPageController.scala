3b84aead94cae05fe79544aeab01a338dea73273
Ang56187 <ax54@live.com>
2019-11-20 08:48:09 +0800

    added basic functionality of chatbox(not fully functional yet)

--- src/main/scala/DS_DrawSomething/controller/LobbyPageController.scala
+++ src/main/scala/DS_DrawSomething/controller/LobbyPageController.scala
@@ -1,8 +1,10 @@
 package DS_DrawSomething.controller
 
 import DS_DrawSomething.Main
+import scalafx.geometry.Insets
 import scalafx.scene.control.{Button, Label, TextArea}
 import scalafx.scene.layout.{FlowPane, VBox}
+import scalafx.scene.text.Text
 import scalafxml.core.macros.sfxml
 
 @sfxml
@@ -13,6 +15,9 @@ class LobbyPageController (private val lblLobbyName:Label,
                            private val btnStartGame:Button,
                            private val txtChat:TextArea,
                            private val btnSubmitChat:Button){
+  //set spacing between chat bubbles
+  vBoxChat.setSpacing(15)
+
 
   def goToLobbyList(): Unit ={
     Main.goToLobbyList()
@@ -22,6 +27,17 @@ class LobbyPageController (private val lblLobbyName:Label,
     Main.goToGamePage()
   }
 
-  lblLobbyName.setText("Lol")
+  def createChatBubble(): Unit ={
+    if (! txtChat.getText.isEmpty) {
+
+      val chatText = new Text(txtChat.getText)
+      chatText.wrappingWidthProperty.set(375)
+      chatText.getStyleClass.add("chat-text")
+      vBoxChat.getChildren.add(chatText)
+    }
+  }
+
+  //use this to set text for label ater
+  //lblLobbyName.setText("Lol")
 
 }
